99d6683a2b05e3bca1865a19e05a7e43493ffe25
Samuel Yoon <yoonsamu@usc.edu>
2024-03-19 23:43:23 +0000

    finished part1!

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,18 +1,48 @@
 #ifndef RECCHECK
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
-
 #endif
-
 #include "equal-paths.h"
 using namespace std;
 
 
 // You may add any prototypes of helper functions here
-
+int depth(Node * root);
 
 bool equalPaths(Node * root)
 {
-    // Add your code below
+  
+  // Add your code below
+  if (root == nullptr) {
+    return true;
+  }
+  //
+  if (root->right == nullptr && root->left == nullptr) {
+    return true;
+  }
+  //
+  bool lhand = equalPaths(root->left);
+  bool rhand = equalPaths(root->right);
 
+  if(root->left == nullptr || root->right == nullptr) {
+    return lhand && rhand;
+  }
+
+  if (rhand && lhand && (depth(root->left) != depth(root->right))) {
+    return false;
+  }
+  return lhand && rhand;
+}
+
+int depth(Node* root) {
+  if (root == nullptr) {
+    return 0;
+  }
+  int left = depth(root->left);
+  int right = depth(root->right);
+  
+  if (left > right) {
+    return left + 1;
+  }
+  return right + 1;
 }
 
