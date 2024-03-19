4b66952f8894383f90256bf4a5aaf4f78da98cc8
Samuel Yoon <yoonsamu@usc.edu>
2024-03-19 23:47:14 +0000

    finished with part1!

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -12,26 +12,33 @@ bool equalPaths(Node * root)
 {
   
   // Add your code below
+  // check for the two leaves on the other levels, which might hold false all the way up (trickle?)
+   // check if root node is empty
   if (root == nullptr) {
     return true;
   }
-  //
+  // check if node is a leaf
   if (root->right == nullptr && root->left == nullptr) {
     return true;
   }
-  //
+  // check for the leaves on both sides
   bool lhand = equalPaths(root->left);
   bool rhand = equalPaths(root->right);
 
+  // check if the leafe is on one side anad empty also
   if(root->left == nullptr || root->right == nullptr) {
     return lhand && rhand;
   }
 
+  // there might be a leaf on both sides
   if (rhand && lhand && (depth(root->left) != depth(root->right))) {
     return false;
   }
+  // only return false if there's only one case where leaves are on different levels
+  // false trickles up
   return lhand && rhand;
 }
+
 
 int depth(Node* root) {
   if (root == nullptr) {
